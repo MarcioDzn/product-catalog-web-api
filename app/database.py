6d07e2c2f87d01c4c5203ff176b3cb2fedc97ba9
MarcioDzn <marcio.roberto0739@gmail.com>
2026-08-23 10:44:00 -0300

    feat: setup project

---
+++ app/database.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
+
+Base = declarative_base()
+
+url = URL.create(
+    drivername=os.getenv("DRIVER"),
+    username=os.getenv("USERNAME"),
+    password=os.getenv("PASSWORD"),
+    host=os.getenv("HOST"),
+    database=os.getenv("DATABASE"),
+    port=int(os.getenv("PORT")),
+)
+
+engine = create_engine(url)
+SessionLocal = sessionmaker(bind=engine)
