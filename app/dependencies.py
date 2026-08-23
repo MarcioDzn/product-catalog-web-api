dc7dfb067cd9a6ab1d9935c6c6faebf60e06cbc6
MarcioDzn <marcio.roberto0739@gmail.com>
2026-08-23 14:54:04 -0300

    feat: add product dependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,8 +1,8 @@
 from fastapi import Depends
 
 from app.database import SessionLocal
-from app.repositories import CategoryRepository, UserRepository
-from app.services import CategoryService, UserService
+from app.repositories import CategoryRepository, ProductRepository, UserRepository
+from app.services import CategoryService, ProductService, UserService
 
 
 def get_session():
@@ -21,3 +21,9 @@ def get_user_service(session=Depends(get_session)):
 def get_category_service(session=Depends(get_session)):
     category_repository = CategoryRepository(session)
     return CategoryService(category_repository)
+
+
+def get_product_service(session=Depends(get_session)):
+    category_repository = CategoryRepository(session)
+    product_repository = ProductRepository(session)
+    return ProductService(product_repository, category_repository)
