d4ed163d7f0bc8343ca95b1bb67ee153d44e91eb
MarcioDzn <marcio.roberto0739@gmail.com>
2026-08-23 15:19:42 -0300

    feat: add product_image repository

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,3 +1,4 @@
 from app.repositories.category import CategoryRepository
 from app.repositories.product import ProductRepository
 from app.repositories.user import UserRepository
+from app.repositories.product_image import ProductImageRepository
---
+++ app/repositories/product_image.py
@@ -0,0 +1,55 @@
+from app.models import ProductImage
+
+
+class ProductImageRepository:
+    def __init__(self, session):
+        self.session = session
+
+    def create(self, product_image_data):
+        product_image_db = ProductImage(
+            product_id=product_image_data.product_id,
+            url=product_image_data.url,
+            is_cover=product_image_data.is_cover,
+        )
+
+        self.session.add(product_image_db)
+        self.session.commit()
+        self.session.refresh(product_image_db)
+
+        return product_image_db
+
+    def get_all_by_product_id(self, product_id):
+        return (
+            self.session.query(ProductImage)
+            .filter(ProductImage.product_id == product_id)
+            .all()
+        )
+
+    def unset_cover(self, image):
+        image.is_cover = False
+        self.session.flush()
+
+    def get_by_id(self, id):
+        return self.session.query(ProductImage).filter(ProductImage.id == id).first()
+
+    def update(self, product_image, product_image_data):
+        updated_data = {}
+
+        for key, value in product_image_data.model_dump(exclude_unset=True).items():
+            if value is not None:
+                updated_data[key] = value
+
+        db_fields = {column.name for column in product_image.__table__.columns}
+
+        for key, value in updated_data.items():
+            if key in db_fields:
+                setattr(product_image, key, value)
+
+        self.session.commit()
+        self.session.refresh(product_image)
+
+        return product_image
+
+    def delete(self, product_image):
+        self.session.delete(product_image)
+        self.session.commit()
