f568f1a83832c7014c7301e749d95af0da8b9d1b
MarcioDzn <marcio.roberto0739@gmail.com>
2026-08-23 12:25:56 -0300

    feat: add category dependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,8 +1,8 @@
 from fastapi import Depends
 
 from app.database import SessionLocal
-from app.services import UserService
-from app.repositories import UserRepository
+from app.repositories import CategoryRepository, UserRepository
+from app.services import CategoryService, UserService
 
 
 def get_session():
@@ -13,7 +13,11 @@ def get_session():
         session.close()
 
 
-def get_user_service(session = Depends(get_session)):
+def get_user_service(session=Depends(get_session)):
     user_repository = UserRepository(session)
     return UserService(user_repository, session)
 
+
+def get_category_service(session=Depends(get_session)):
+    category_repository = CategoryRepository(session)
+    return CategoryService(category_repository)
--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-from app.routes import user_router
+
+from app.routes import category_router, user_router
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(category_router)
+
 
 @app.get("/")
 def home():
