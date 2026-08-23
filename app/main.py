9dbf347b2af13d749486bfcf90068346cb1c8238
MarcioDzn <marcio.roberto0739@gmail.com>
2026-08-23 18:54:53 -0300

    chore: setup cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes import (
     auth_router,
@@ -9,6 +10,15 @@ from app.routes import (
 )
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(user_router)
 app.include_router(category_router)
