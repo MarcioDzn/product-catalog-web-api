b4b699ab7181b4e331e303fa4f68bf1aef54ce02
MarcioDzn <marcio.roberto0739@gmail.com>
2026-08-23 11:38:23 -0300

    feat: add custom exception classes

---
+++ app/exceptions.py
@@ -0,0 +1,11 @@
+class NotFoundError(Exception):
+    pass
+
+class UniqueFieldError(Exception):
+    pass
+
+class ConflictError(Exception):
+    pass
+
+class UnprocessableEntityError(Exception):
+    pass
