5c44e84f8ce81ea0e196939021f25638254db507
MarcioDzn <marcio.roberto0739@gmail.com>
2026-08-23 10:59:36 -0300

    chore: add users table model

---
+++ app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, DateTime, Integer, String, func
+
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
